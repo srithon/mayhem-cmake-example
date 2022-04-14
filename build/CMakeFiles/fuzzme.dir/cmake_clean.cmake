400d32114c828b3da9d930521fb6c9a2dceee9f9
Alex Brewer <abrewer@forallsecure.com>
2022-04-14 17:44:08 -0400

    Update Mayhemfile

---
+++ build/CMakeFiles/fuzzme.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuzzme.dir/fuzzme.c.o"
+  "CMakeFiles/fuzzme.dir/fuzzme.c.o.d"
+  "fuzzme"
+  "fuzzme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fuzzme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
